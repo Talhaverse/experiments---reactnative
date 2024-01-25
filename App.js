7a83832384ee27dee63745fca3fc8d1610604e9e
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-01-25 19:58:59 +0500

    daily push

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! testing with talal</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Open up App.js to start working on your app! will use this for my experiments etc lambi </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
