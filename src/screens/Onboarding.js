1b30451fd2c715d20e9573ac7e7f15fb4a17a124
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-02-01 15:18:09 +0500

    folder str

--- src/screens/Onboarding.js
+++ src/screens/Onboarding.js
@@ -1,30 +1,16 @@
-import { View,StyleSheet,SafeAreaView } from 'react-native'
+import { View,StyleSheet,SafeAreaView,Text } from 'react-native'
 import React from 'react'
 import { Animated } from 'react-native'
-import data from '../../Data/Data'
-import RenderItem from '../../Components/RenderItem'
+// import data from '../Data/Data'
 
+// import RenderItem from '../Components/RenderItem'
 
 const Onboarding = () => {
   return (
     <SafeAreaView style={styles.container}>
      
 
-<Animated.Flatlist
- data={data} 
- renderItem={({item,index}) => {
-  return <RenderItem />
-}}
-keyExtractor={item => item.id}
-scrollEventThrottle={16}
-horizontal={true}
-bounces={false}
-pagingEnabled={true}
-showsHorizontalScrollIndicator={false}
-
-
-/>
-
+<Text>ff</Text>
     </SafeAreaView>
   )
 }
