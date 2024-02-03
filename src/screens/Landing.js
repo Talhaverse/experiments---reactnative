075716a921895d2dfeed5e0b34c536809729163c
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-02-03 13:43:52 +0500

    ...

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,23 +1,12 @@
-import { View, Text,StyleSheet,Dimensions,SafeAreaView,Platform } from 'react-native'
+import { View, Text,SafeAreaView,ImageBackground } from 'react-native'
 import React from 'react'
-import LottieView from 'lottie-react-native';
-import tw from 'twrnc'
 
-
-const {width,height} = Dimensions.get('window');
-
-
-export default function Landing() {
-  return(
+const Landing = () => {
+  return (
     <SafeAreaView>
-
+      <Text>Signup login Page  </Text>
     </SafeAreaView>
   )
 }
 
-style = StyleSheet.create({
-  container: {
-    flex:1,
-    alignItems: 'center'
-  }
-})
+export default Landing
