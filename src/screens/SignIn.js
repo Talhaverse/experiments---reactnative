f1d46a311deaf7a3f1e8ad9020268a188271072d
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-02-04 12:18:55 +0500

    signin and signup  page

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,35 +1,66 @@
-import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
+import tw from "twrnc";
+import { useNavigation } from "@react-navigation/native";
+import LottieView from "lottie-react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const SignIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignIn = () => {
     // Add your sign-in logic here
-    console.log('Signing in with:', email, password);
+    console.log("Signing in with:", email, password);
   };
 
+  const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <TouchableOpacity style={styles.signInButton} onPress={handleSignIn}>
-        <Text style={styles.buttonText}>Sign In</Text>
-      </TouchableOpacity>
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require("../../assets/4873152.jpg")}
+      >
+        <KeyboardAwareScrollView>
+          <LottieView
+            source={require("../../assets/animations/Animation5.json")}
+            autoPlay
+            loop
+            style={{ height: 400, width: 400 }}
+          />
+
+          <View style={styles.container}>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+            <TouchableOpacity
+              style={styles.signInButton}
+              onPress={handleSignIn}
+            >
+              <Text style={styles.buttonText}>Sign In</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAwareScrollView>
+      </ImageBackground>
     </View>
   );
 };
@@ -37,30 +68,31 @@ const SignIn = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     paddingHorizontal: 16,
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 50,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1",
     borderRadius: 10,
     marginBottom: 20,
     padding: 10,
     fontSize: 16,
+    borderWidth: 1,
   },
   signInButton: {
-    backgroundColor: '#3498db',
+    backgroundColor: "#3498db",
     borderRadius: 10,
     padding: 15,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,42 +1,74 @@
-import React, { useState } from 'react';
-import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
+import tw from "twrnc";
+import { useNavigation } from "@react-navigation/native";
+import LottieView from "lottie-react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const SignUp = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignUp = () => {
     // Add your sign-up logic here
-    console.log('Signing up with:', name, email, password);
+    console.log("Signing up with:", name, email, password);
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Name"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <TouchableOpacity style={styles.signUpButton} onPress={handleSignUp}>
-        <Text style={styles.buttonText}>Sign Up</Text>
-      </TouchableOpacity>
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require("../../assets/4873152.jpg")}
+      >
+        <KeyboardAwareScrollView>
+          <LottieView
+            source={require("../../assets/animations/Animation6.json")}
+            autoPlay
+            loop
+            style={{ height: 300, width: 400 }}
+          />
+
+          <View style={styles.container}>
+            <TextInput
+              style={styles.input}
+              placeholder="Name"
+              value={name}
+              onChangeText={(text) => setName(text)}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+
+            <TouchableOpacity
+              style={styles.signUpButton}
+              onPress={handleSignUp}
+            >
+              <Text style={styles.buttonText}>Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+        </KeyboardAwareScrollView>
+      </ImageBackground>
     </View>
   );
 };
@@ -44,30 +76,31 @@ const SignUp = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     paddingHorizontal: 16,
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 50,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1",
     borderRadius: 10,
     marginBottom: 20,
     padding: 10,
     fontSize: 16,
+    borderWidth: 1,
   },
   signUpButton: {
-    backgroundColor: '#27ae60', // Complementary color to the sign-in form
+    backgroundColor: "#27ae60", // Complementary color to the sign-in form
     borderRadius: 10,
     padding: 15,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
