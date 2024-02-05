69427831248f3b0bd73982a96978e5ddb0a2b19a
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-02-05 13:43:11 +0500

    homepage

---
+++ src/screens/HomePage.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <View>
+      <Text>HomePage</Text>
+    </View>
+  )
+}
+
+export default HomePage
