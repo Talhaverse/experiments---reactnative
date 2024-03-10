a9be4e3bd7258181f289c6c17def7ae2277b7c41
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-03-11 02:42:38 +0500

    .

--- config/firebase.js
+++ config/firebase.js
@@ -2,6 +2,7 @@
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
