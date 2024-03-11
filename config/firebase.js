d6b1ae3fab322b2ee426c2b62f3416db4226e22b
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-03-11 17:38:33 +0500

    trying to persist state

--- config/firebase.js
+++ config/firebase.js
@@ -7,7 +7,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
 
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
